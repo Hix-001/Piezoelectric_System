b815147db1bc5c0bc2a1028d879463709b4c5f2c
hix-001 <harsh.25scs1003004095@gmail.com>
2026-04-13 06:31:38 +0530

    fix: index.html & deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Piezoelectric_System/',
-
   plugins: [react()],
-
-  optimizeDeps: {
-    include: ['three', '@react-three/fiber', '@react-three/drei', 'postprocessing'],
-  },
-
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three:   ['three'],
-          r3f:     ['@react-three/fiber', '@react-three/drei'],
-          postpro: ['@react-three/postprocessing', 'postprocessing'],
-          framer:  ['framer-motion'],
-        },
-      },
-    },
+    // We are removing the manualChunks object to satisfy Vite 8 requirements
   },
 })
