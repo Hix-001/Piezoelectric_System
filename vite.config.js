d45675d2cfe84276a844bdbb9854efce9dc26e8d
hix-001 <harsh.25scs1003004095@gmail.com>
2026-04-13 06:28:11 +0530

    fixed punctuation

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pizeoelectric_System/',
+  base: '/Piezoelectric_System/',
 
   plugins: [react()],
 
